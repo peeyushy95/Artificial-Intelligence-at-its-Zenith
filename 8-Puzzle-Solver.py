3ffe530eb1bbd4cf4d8df5091bcb8c12d227dd00
peeyushy95 <peeyushy95@gmail.com>
2017-03-30 23:10:13 +0530

    DFS

--- 8-Puzzle-Solver.py
+++ 8-Puzzle-Solver.py
@@ -8,20 +8,16 @@ Created on Wed Mar 29 21:22:03 2017
 __author__ = 'Peeyush Yadav'
 
 import copy
-
+import sys
 # Where 0 denotes the blank tile or space.
-initial_state = [3,1,2, 
-                 6,4,5, 
-                 7,8,0]
-goal_state = [0,1,2,
-              3,4,5,
-              6,7,8]
+initial_state = "312640785"
+goal_state =    "012345678"
 
 def checkSolvability(initial_state):
     inversion = 0;
     for i in range(0,len(initial_state)-1):
         for j in range(i+1,len(initial_state)):
-            if initial_state[i] != 0 and initial_state[j] != 0 and initial_state[i] > initial_state[j] :
+            if initial_state[i] != '0' and initial_state[j] != '0' and initial_state[i] > initial_state[j] :
                 inversion += 1    
 
     if inversion % 2 == 0 :
@@ -36,70 +32,76 @@ def DFS_Search(initial_state, goal_state) :
     
     while stack.isEmpty() == False :
         current_state = stack.pop()
+        explored_states.append(current_state.config)
         #print("Inside Stack", current_state.config)
         if current_state.config == goal_state:
             printPathtoGoal(current_state)
             return True;
               
-        neighbour_states = explore_next_states(current_state, explored_states)
+        neighbour_states = explore_next_states(current_state, explored_states,stack.item())
         for states in neighbour_states:
             stack.push(states)
-        if len(explored_states) > 3 :
+        if len(explored_states) % 10000 == 0 :
             print(len(explored_states))
-        
-        explored_states.append(current_state.config)
+    
             
     return False
     
-def explore_next_states(current_state, explored_states):
+def explore_next_states(current_state, explored_states,stack):
     
     new_states = []
-
-    if(current_state.config not in explored_states):  
         
-        new_state = move('down', current_state)
-        if new_state[0] == True and new_state[1].config not in explored_states:
-            new_states.append(new_state[1])
-            new_state[1].parent = current_state
+    new_state = move('down', current_state)
+    if new_state[0] == True and any(x for x in explored_states if x == new_state[1].config) == False and any(x for x in stack if x.config == new_state[1].config) == False:
+        new_states.append(new_state[1])
+        new_state[1].parent = current_state
+        #print(new_state[1].config)
+
+    new_state = move('right', current_state)
+    if new_state[0] == True and any(x for x in explored_states if x == new_state[1].config) == False and any(x for x in stack if x.config == new_state[1].config) == False:
+        new_states.append(new_state[1])
+        new_state[1].parent = current_state
+        #print(new_state[1].config)
+         
+    new_state = move('up', current_state)
+    if new_state[0] == True and any(x for x in explored_states if x == new_state[1].config) == False and any(x for x in stack if x.config == new_state[1].config) == False:
     
-        new_state = move('right', current_state)
-        if new_state[0] == True and new_state[1].config not in explored_states:
-            new_states.append(new_state[1])
-            new_state[1].parent = current_state
-             
-        new_state = move('up', current_state)
-        if new_state[0] == True and new_state[1].config not in explored_states:
-            new_states.append(new_state[1])
-            new_state[1].parent = current_state
-            
-        new_state = move('left', current_state)    
-        if new_state[0] == True and new_state[1].config not in explored_states:
-            new_states.append(new_state[1])
-            new_state[1].parent = current_state
-            
+        new_states.append(new_state[1])
+        new_state[1].parent = current_state
+        #print(new_state[1].config)
+        
+    new_state = move('left', current_state)    
+    if new_state[0] == True and any(x for x in explored_states if x == new_state[1].config) == False and any(x for x in stack if x.config == new_state[1].config) == False:
+    
+        new_states.append(new_state[1])
+        new_state[1].parent = current_state
+        #print(new_state[1].config)
+        
+    #sys.exit()
     return new_states;    
 
 def move(direction,state):
     
-    new_state = copy.deepcopy(state)
-    index = new_state.config.index( 0 )
+    new_state = copy.copy(state)
+    index = new_state.config.index('0')
+    state_list = list(new_state.config)
     if direction == 'up':
         if index not in [0,1,2]:
-           new_state.config[index - 3],new_state.config[index] = new_state.config[index],new_state.config[index-3]          
+            state_list[index - 3],state_list[index] = state_list[index],state_list[index-3]
     elif direction == 'down':
         if index not in [6,7,8]:
-            new_state.config[index + 3],new_state.config[index] = new_state.config[index],new_state.config[index + 3]  
+            state_list[index + 3],state_list[index] = state_list[index],state_list[index + 3]  
     elif direction == 'left':
         if index not in [0,3,6]:
-            new_state.config[index - 1],new_state.config[index] = new_state.config[index],new_state.config[index - 1]  
+            state_list[index - 1],state_list[index] = state_list[index],state_list[index - 1]  
     else:
         if index not in [2,5,8]:
-            new_state.config[index + 1],new_state.config[index] = new_state.config[index],new_state.config[index + 1]
+            state_list[index + 1],state_list[index] = state_list[index],state_list[index + 1]
 
+    new_state.config = ''.join(state_list)
     return (new_state.config != state.config,new_state)
 
 def printPathtoGoal(current_state):
-    print("Hello")
     stackTrace = Stack()
     while True:
         stackTrace.push(current_state.config)
@@ -130,6 +132,9 @@ class Stack:
         
      def size(self):
          return len(self.items)
+    
+     def item(self):
+         return  self.items
 
 class State :
     def __init__(self, config, parent, depth):
@@ -144,7 +149,7 @@ if __name__ == "__main__":
         if DFS_Search(initial_state_info, goal_state) == True:
             print("Solved")
         else:
-            print("DFS FAiled")
+            print("DFS Failed")
     else:
         print ("Can't be Solved")
     
