46576d6ec6f902fc4aedb89ae07892ad34bc72b7
peeyushy95 <peeyushy95@gmail.com>
2017-04-12 22:47:44 +0530

    Using Scaling and Matrix

--- Linear Regression/AnalyticalApproach(Matrix).py
+++ Linear Regression/AnalyticalApproach(Matrix).py
@@ -7,8 +7,8 @@ def load_dataset(x,y):
     with open('data.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            x.append(float(row[0]))
-            y.append(float(row[1]))
+            x.append([1, float(row[0])])
+            y.append([float(row[1])])
     
    
 #    print ("x = ",x)
@@ -17,7 +17,7 @@ def load_dataset(x,y):
 
 def plot_dataset(x,y):
     
-    plt.scatter(x,y)
+    plt.scatter([row[1] for row in x ],y)
     plt.title('Scatter plot')
     plt.xlabel('x')
     plt.ylabel('y')
@@ -28,31 +28,39 @@ class Linear_Regression :
     def __init__(self,dataset_x,dataset_y):
         self.x = dataset_x
         self.y = dataset_y
+    
         
-    def analytical_approach(self):    
-        self.betaOne = 0.0
-        self.betaZero = 0.0
-        n = len(self.x)
+    def analytical_approach(self):
+        self.scale_data()
+        xMatrix = np.matrix(self.x)
+        yMatrix = np.matrix(self.y)
+        xMatrixT = np.transpose(xMatrix)
+        beta = (((xMatrixT.dot(xMatrix)).getI()).dot(xMatrixT)).dot(yMatrix)
+        self.betaOne = float(beta[1])
+        self.betaZero = float(beta[0])
+        print("Beta: ",self.betaZero,self.betaOne)    
+   
         
-        sum_x = sum(self.x)
-        sum_y = sum(self.y)
-        sum_xy = 0
-        sum_x2 = 0
-        for i in range(n):
-            sum_x2 += self.x[i]*self.x[i]
-        for i in range(n):
-            sum_xy += self.x[i]*self.y[i]
+    def scale_data(self):
+        x = np.array(self.x)
+        y = np.array(self.y)
+        meanX = x[:,1].mean()
+        meanY = y[:,0].mean()
+        stdX =  x[:,1].std()
+        stdY =  y[:,0].std()
         
-        self.betaOne = (sum_xy - (1.0/n*sum_x*sum_y))/(sum_x2 - (1.0/n*(sum_x*sum_x)))
-        self.betaZero = 1.0/n*(sum_y  - self.betaOne*sum_x)
-           
+        for ind in range(len(x)):
+            self.x[ind][1] = (self.x[ind][1] - meanX)/stdX
+            self.y[ind][0] = (self.y[ind][0] - meanY)/stdY
+
+
     def R_function(self):
         self.predicted_y = []
         tempsum = 0.0
         n = len(self.x)
         for i in range(n):
-            self.predicted_y.append(self.betaZero+self.betaOne*self.x[i])
-            diff = self.y[i] - self.predicted_y[i]
+            self.predicted_y.append(self.betaZero+self.betaOne*self.x[i][1])
+            diff = self.y[i][0] - self.predicted_y[i]
             tempsum += diff*diff
         R = 1/(2.0*n)*tempsum
         print ("R = ",R)
@@ -61,8 +69,8 @@ class Linear_Regression :
         
     def plot_regression_line(self):
         self.R_function()
-        plt.scatter(self.x,self.y)
-        plt.plot(self.x,self.predicted_y, '--r')
+        plt.scatter([row[1] for row in self.x ],self.y)
+        plt.plot([row[1] for row in self.x ],self.predicted_y, '--r')
         plt.title('Plot Regression Line')
         plt.xlabel('x')
         plt.ylabel('y')
