c028472b73c22a84a6e5afd8f5e2ab01cb4ce387
peeyushy95 <peeyushy95@gmail.com>
2017-05-16 11:41:23 +0530

    Data Cleaning

---
+++ Data Preprocessing/Cleaning/missing_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import Imputer
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
